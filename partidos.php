011f175764e871d53a9b3be4282caf3d73e36ce9
lancasterjones <lancasterjones@gmail.com>
2015-02-17 00:54:33 -0600

    Prueba PHP

--- partidos.php
+++ partidos.php
@@ -100,6 +100,7 @@
           </tr></thead>
           <tbody>
             <?php
+            echo "Inicio Body";
               if (mysqli_num_rows($result) > 0) {
                 while($row = mysqli_fetch_array($result))
               {
