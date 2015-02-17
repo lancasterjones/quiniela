bd6e3c6a28d5a8350146398fc67917ad931caf84
lancasterjones <lancasterjones@gmail.com>
2015-02-16 23:22:17 -0600

    Menu

--- partidos.php
+++ partidos.php
@@ -22,9 +22,9 @@ Partidos
         </div>
         <div id="navbar" class="navbar-collapse collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
+            <li class="active"><a href="#">Inicio</a></li>
+            <li><a href="#partidos">Partidos</a></li>
+            <li><a href="#tabla">Tabla</a></li>
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
