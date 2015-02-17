2f47a41a128e8ece93ba7c67913997785c3b0812
lancasterjones <lancasterjones@gmail.com>
2015-02-16 23:45:44 -0600

    Elementos faltantes nav

--- partidos.php
+++ partidos.php
@@ -11,7 +11,9 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
 
 </head>
-<div class="navbar-header">
+<nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
