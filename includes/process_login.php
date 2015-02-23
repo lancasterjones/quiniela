6e3bcf7123b63d42cc2f879f6193cca4ac23bedb
lancasterjones <lancasterjones@gmail.com>
2015-02-22 22:15:23 -0600

    Rutas en Process Login

--- includes/process_login.php
+++ includes/process_login.php
@@ -1,10 +1,10 @@
 <?php
-include_once '/includes/db_connect.php';
-include_once '/includes/functions.php';
-
-echo "<h1>Process Login </h1>";
+include_once 'includes/db_connect.php';
+include_once 'includes/functions.php';
 
 sec_session_start(); // Our custom secure way of starting a PHP session.
+
+echo "<h1>Process Login </h1>";
 
 if (isset($_POST['email'], $_POST['p'])) {
     $email = $_POST['email'];
