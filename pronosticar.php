98c01937214ccdb3d5653b58de286e41561b62c9
lancasterjones <lancasterjones@gmail.com>
2015-02-23 02:34:29 -0600

    Debug 6
    Semicolon

--- pronosticar.php
+++ pronosticar.php
@@ -84,7 +84,7 @@ sec_session_start();
               $result = mysqli_query($conn, $sql);
 
               if (mysqli_num_rows($result) = 0) {
-                echo "<br>Sin Resultados";  }
+                echo "<br>Sin Resultados";  } ;
             /*    else {
                     while($row = mysqli_fetch_array($result))
                     {
