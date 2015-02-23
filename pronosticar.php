08bf022d5d6ff495b6e1558623e8aebfc23549aa
lancasterjones <lancasterjones@gmail.com>
2015-02-23 02:29:08 -0600

    Debug 3

--- pronosticar.php
+++ pronosticar.php
@@ -81,8 +81,8 @@ sec_session_start();
                       FROM pronosticos_prueba
                       WHERE id_partido = '$id_partido' AND
                       id_usuario =  '$id_usuario' ";
-                      /*
               $result = mysqli_query($conn, $sql);
+              /*
               if (mysqli_num_rows($result) > 0) {
           while($row = mysqli_fetch_array($result))
             {
