f3166302058950362026795c7894b95e6f5d10ca
lancasterjones <lancasterjones@gmail.com>
2015-02-23 03:52:52 -0600

    No mostrar tabla si Octavos terminaron

--- partidos.php
+++ partidos.php
@@ -72,7 +72,11 @@ sec_session_start();
           <p><strong>2 Puntos </strong>por acertar en marcador</p>
           <p><strong>3 Puntos </strong>máximo por partido</p>
         </div>
-
+        <?php
+        $sql = "SELECT * FROM view_partidos
+        WHERE ronda = 'Octavos'
+        AND (view_partidos.fecha BETWEEN DATE_ADD(NOW(), INTERVAL 1 HOUR) AND DATE_ADD(view_partidos.fecha, INTERVAL 0 HOUR))";
+        if (mysqli_num_rows($result) > 0) { ?>;
         <!-- Table -->
         <table class='table'>
           <thead><tr>
@@ -83,9 +87,7 @@ sec_session_start();
           </tr></thead>
           <tbody>
               <?php
-              $sql = "SELECT * FROM view_partidos
-WHERE ronda = 'Octavos'
-AND (view_partidos.fecha BETWEEN DATE_ADD(NOW(), INTERVAL 1 HOUR) AND DATE_ADD(view_partidos.fecha, INTERVAL 0 HOUR))";
+
               $result = mysqli_query($conn, $sql);
               if (mysqli_num_rows($result) > 0) {
           while($row = mysqli_fetch_array($result))
@@ -101,6 +103,7 @@ AND (view_partidos.fecha BETWEEN DATE_ADD(NOW(), INTERVAL 1 HOUR) AND DATE_ADD(v
                 ?>
           </tbody>
         </table>
+        <?php } else { }; ?>
       </div>
       <br>
 
