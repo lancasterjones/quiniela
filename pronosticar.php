4127e9e11bd185ad48c94b75bc7417edfd4a8635
lancasterjones <lancasterjones@gmail.com>
2015-02-23 03:06:37 -0600

    WHERE id_partido

--- pronosticar.php
+++ pronosticar.php
@@ -80,8 +80,8 @@ sec_session_start();
               /* $id_usuario = htmlentities($_SESSION['user_id']) */;
               $sql = "SELECT *
                       FROM pronosticos_prueba
-                      /* WHERE id_partido = '$id_partido' AND
-                      id_usuario =  '$id_usuario' */ ";
+                      WHERE id_partido = '$id_partido'
+                      /* AND id_usuario =  '$id_usuario' */ ";
               $result = mysqli_query($conn, $sql);
 
               if (mysqli_num_rows($result) > 0) {
