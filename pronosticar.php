1aa64e254835d438fe0e8981172e03b389dd0bb7
lancasterjones <lancasterjones@gmail.com>
2015-02-23 03:30:34 -0600

    Debug multiplicador

--- pronosticar.php
+++ pronosticar.php
@@ -77,6 +77,7 @@ sec_session_start();
               <?php
               $accion = "Cambiar";
               $id_partido =  ($_GET['p']/112);
+              echo $id_partido;
               $id_usuario = htmlentities($_SESSION['user_id']);
               /* $id_usuario = htmlentities($_SESSION['user_id']) */;
               $sql = "SELECT *
