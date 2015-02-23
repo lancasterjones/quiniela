caf537fe374aa5d26b5345887ad2f0325751bcfe
lancasterjones <lancasterjones@gmail.com>
2015-02-23 00:26:28 -0600

    Menú Logout

--- partidos.php
+++ partidos.php
@@ -50,6 +50,10 @@ sec_session_start();
             <li class="active"><a href="/partidos.php">Partidos</a></li>
             <li><a href="/pronosticos.php">Pronósticos</a></li>
             <li><a href="/tabla.php">Tabla</a></li>
+            <li style:"float:right;"><?php if (login_check($mysqli) == true) {
+                echo "<a href='includes/logout.php' >Salir</a>"
+            } ?>
+            </li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
@@ -169,7 +173,7 @@ sec_session_start();
     <?php else : ?>
       <br><br>
         <p>
-        <?php include 'includes/mensaje_no_autorizado.php'; ?>  
+        <?php include 'includes/mensaje_no_autorizado.php'; ?>
         </p>
     <?php endif; ?>
     </div>
